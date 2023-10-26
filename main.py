5bd44970f33526873f5f7a6b9f7c2a189c73104c
Bruno César <79723563+brunodeev@users.noreply.github.com>
2023-10-26 19:50:08 -0300

    Questao 2 Pronta

--- main.py
+++ main.py
@@ -13,23 +13,22 @@ class Funcionario:
     def __init__(self):
         nome = ""
         cargo = ""
-        desconto = 0
-        salarioBruto = 0
-        salarioLiquido = 0
+        desconto = 0.00
+        salarioBruto = 0.00
+        salarioLiquido = 0.00
         horasTrabalhadas = 0
 
-def gravarDados(funcionario, numero) :
+def gravarDados(funcionario) :
 
     f = open('folha_pag.txt', 'a', encoding="utf-8")
-    f.write(f"----------- Funcionario {numero} ---------\n")
     f.write("Nome: " + funcionario.nome + "\n")
     f.write("Cargo: " + funcionario.cargo + "\n")
-    f.write("Salário Bruto: R$ " + str(funcionario.salarioBruto) + "\n")
-    f.write("Salário Liquido: R$ " + str(funcionario.salarioLiquido) + "\n")
+    f.write("Salário Bruto: R$ " + "{:.2f}".format(funcionario.salarioBruto) + "\n")
+    f.write("Salário Liquido: R$ " + "{:.2f}".format(funcionario.salarioLiquido) + "\n")
     f.write("Horas Trabalhadas: " + str(funcionario.horasTrabalhadas) + "\n")
+    f.write("------------------------")
     f.write("\n")
     f.close()
-    numero += 1
 
 
 def imprimirDados():
@@ -39,12 +38,12 @@ def imprimirDados():
     global totalSalarioLiquido
 
     print('\n--------------------------------------------')
-    print("Total de descontos: ", totalDescontos)
-    print("Total de salário bruto: ", totalSalarioBruto)
-    print("Total de salário líquido: ", totalSalarioLiquido)
+    print("Total de descontos: R$ " + "{:.2f}".format(totalDescontos))
+    print("Total de salário bruto: R$ " + "{:.2f}".format(totalSalarioBruto))
+    print("Total de salário líquido: R$ " + "{:.2f}".format(totalSalarioLiquido))
     print('--------------------------------------------\n')
 
-def cadastroFuncionario(numero) :
+def cadastroFuncionario() :
     
     funcionario = Funcionario()
     global totalDescontos
@@ -57,8 +56,8 @@ def cadastroFuncionario(numero) :
     funcionario.salarioBruto = salario
     funcionario.desconto = calcularDesconto(salario)
     funcionario.salarioLiquido = calcularImposto(salario)
-    funcionario.horasTrabalhadas = int(input("Digite as horas: "))
-    gravarDados(funcionario, numero)
+    funcionario.horasTrabalhadas = int(input("Digite as horas de trabalho: "))
+    gravarDados(funcionario)
     totalDescontos += funcionario.desconto
     totalSalarioBruto += funcionario.salarioBruto
     totalSalarioLiquido += funcionario.salarioLiquido
@@ -76,42 +75,122 @@ def calcularImposto(salario):
         
 def calcularDesconto(salario):
     if salario <= 1500.00:
-            return 0.0
+            return 0.00
     elif salario <= 3000.00:
-        return salario * 0.15
+        resultado = salario * 0.15
+        resultadoFormatado = float("{:.2f}".format(resultado))
+        return resultadoFormatado
     elif salario <= 5000.00:
-        return salario * 0.20
+        resultado = salario * 0.20
+        resultadoFormatado = float("{:.2f}".format(resultado))
+        return resultadoFormatado
     else:
-        return salario * 0.27
+        resultado = salario * 0.27
+        resultadoFormatado = float("{:.2f}".format(resultado))
+        return resultadoFormatado
+    
+def loading(tempo) :
+    value = 0
+    while value <= tempo :
+        print("")
+        time.sleep(0.08)
+        os.system('clear')
+        print("/")
+        time.sleep(0.08)
+        os.system('clear')
+        print("--")
+        time.sleep(0.08)
+        os.system('clear')
+        print("\x5c")
+        time.sleep(0.08)
+        os.system('clear')
+        value += 1
+        
+def lerArquivo() :
+    with open("folha_pag.txt", "r", encoding="utf-8") as arquivo :
+        texto = arquivo.readlines()
+        
+    for frase in texto:
+        if "Nome" or "Cargo" or "Salário Bruto" or "Salário Liquido" or "Horas Trabalhadas" or "-" in frase:
+            print(frase)
 
 
 def menu() :
-    f = open('folha_pag.txt', 'w', encoding="utf-8")
-    f.truncate()
+    os.system('clear')
+    if os.stat('folha_pag.txt').st_size != 0 :
+        decisao = input('Deseja continuar no arquivo já existente (s/n)? ')
+        
+        if decisao == 'n' or decisao == 'N':
+            os.system('clear')
+            decisao2 = input('O arquivo atual será apagado para iniciar outro, tem certeza (s/n)? ')
+            
+            if decisao2 == 's' or decisao == 'S':
+                os.system('clear')
+                f = open('folha_pag.txt', 'w', encoding='utf-8')
+                f.truncate()
+                
+                os.system('clear')
+            else:
+                os.system('clear')
+        else:
+            os.system('clear')
+    else:
+        os.system('clear')
     
-    numero = 1
     while True:
-        print("\n-------------- MENU ---------------")
-        print(" 1 - Cadastrar funcionário")
-        print(" 2 - Calcular imposto de renda")
-        print(" 99 - Fechar o programa")
-        print("-----------------------------------")
+        print(
+        """
+|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|
+|                    MENU                     |
+|---------------------------------------------|
+|                                             |
+|  1 - Cadastrar funcionário                  |
+|  2 - Visualizar dados gerais                |
+|  3 - Visualizar relatório dos funcionários  |
+|  0 - Fechar o programa                      |
+|                                             |
+|_____________________________________________|
+        """
+        )
 
         opcao = int(input("Digite alguma opção: "))
+        
         if opcao == 1 :
             os.system('clear')
-            cadastroFuncionario(numero)
-            numero += 1
-            time.sleep(2)
+            
+            cadastroFuncionario()
+            loading(3)
+            os.system('clear')
             print("\nCadastrado com sucesso!")
             time.sleep(2)
             os.system('clear')
+                   
         elif opcao == 2 :
             os.system('clear')
-            imprimirDados()
-            time.sleep(6)
+            loading(2)
+            
+            if os.stat('folha_pag.txt').st_size == 0 :
+                print('O arquivo está vazio, cadastre algum funcionário!')
+                time.sleep(3)
+                os.system('clear')
+            else :  
+                imprimirDados()
+                time.sleep(6)
+                os.system('clear')
+                
+        elif opcao == 3 :
             os.system('clear')
-        elif opcao == 99 :
+            loading(2)
+            
+            if os.stat('folha_pag.txt').st_size == 0 :
+                print('O arquivo está vazio, cadastre algum funcionário!')
+                time.sleep(3)
+                os.system('clear')
+            else :  
+                lerArquivo()
+                time.sleep(6)
+                os.system('clear')
+        elif opcao == 0 :
             break
 
 
