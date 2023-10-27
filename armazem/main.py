2d26d48b10bc8c8177895b500b9765b8ab23a60d
Bruno César <79723563+brunodeev@users.noreply.github.com>
2023-10-27 10:55:14 -0300

    FINALIZADO

--- armazem/main.py
+++ armazem/main.py
@@ -1,87 +1,139 @@
-mercadorias = [
-    {"produto": "Maçã", "preco": 1.00, "quantidade": 10},
-    {"produto": "Banana", "preco": 0.30, "quantidade": 20},
-    {"produto": "Laranja", "preco": 0.80, "quantidade": 15},
-    {"produto": "Uva", "preco": 2.00, "quantidade": 5},
-    {"produto": "Morango", "preco": 8.00, "quantidade": 3},
-    {"produto": "Abacaxi", "preco": 4.00, "quantidade": 2},
-    {"produto": "Pera", "preco": 1.00, "quantidade": 7},
-    {"produto": "Melancia", "preco": 20.00, "quantidade": 1},
-    {"produto": "Mamão", "preco": 4.00, "quantidade": 4},
-    {"produto": "Limão", "preco": 0.50, "quantidade": 12},
-    {"produto": "Cenoura", "preco": 0.50, "quantidade": 8},
-    {"produto": "Batata", "preco": 1.00, "quantidade": 10},
-    {"produto": "Tomate", "preco": 1.50, "quantidade": 9},
-    {"produto": "Cebola", "preco": 1.00, "quantidade": 8},
-    {"produto": "Alho", "preco": 0.50, "quantidade": 20},
-    {"produto": "Pimentão", "preco": 1.00, "quantidade": 6},
-    {"produto": "Brócolis", "preco": 1.50, "quantidade": 5},
-    {"produto": "Couve", "preco": 1.00, "quantidade": 4},
-    {"produto": "Alface", "preco": 2.00, "quantidade": 7},
-    {"produto": "Espinafre", "preco": 2.00, "quantidade": 5},
-    {"produto": "Aipo", "preco": 2.00, "quantidade": 3},
-    {"produto": "Pepino", "preco": 1.00, "quantidade": 8},
-    {"produto": "Abobrinha", "preco": 1.50, "quantidade": 6},
-    {"produto": "Milho", "preco": 1.00, "quantidade": 10},
-    {"produto": "Ervilha", "preco": 2.00, "quantidade": 4},
-    {"produto": "Feijão", "preco": 4.00, "quantidade": 2},
-    {"produto": "Arroz", "preco": 2.00, "quantidade": 3},
-    {"produto": "Massa", "preco": 2.00, "quantidade": 5},
-    {"produto": "Pão", "preco": 5.00, "quantidade": 6},
-    {"produto": "Leite", "preco": 2.00, "quantidade": 10},
-    {"produto": "Queijo", "preco": 5.00, "quantidade": 3},
-    {"produto": "Iogurte", "preco": 1.00, "quantidade": 5},
-    {"produto": "Manteiga", "preco": 5.00, "quantidade": 4},
-    {"produto": "Ovo", "preco": 1.00, "quantidade": 30},
-    {"produto": "Frango", "preco": 8.00, "quantidade": 2},
-    {"produto": "Carne bovina", "preco": 20.00, "quantidade": 3},
-    {"produto": "Carne de porco", "preco": 10.00, "quantidade": 2},
-    {"produto": "Peixe", "preco": 15.00, "quantidade": 4},
-    {"produto": "Camarão", "preco": 40.00, "quantidade": 1},
-    {"produto": "Lagosta", "preco": 60.00, "quantidade": 1},
-    {"produto": "Salmão", "preco": 30.00, "quantidade": 2},
-    {"produto": "Atum", "preco": 4.00, "quantidade": 3},
-    {"produto": "Sardinha", "preco": 2.00, "quantidade": 5},
-    {"produto": "Bacon", "preco": 8.00, "quantidade": 3},
-    {"produto": "Linguiça", "preco": 5.00, "quantidade": 4},
-    {"produto": "Presunto", "preco": 10.00, "quantidade": 2},
-    {"produto": "Salsicha", "preco": 3.00, "quantidade": 5},
-    {"produto": "Azeite de oliva", "preco": 10.00, "quantidade": 2},
-    {"produto": "Óleo de cozinha", "preco": 5.00, "quantidade": 2},
-    {"produto": "Vinagre", "preco": 2.00, "quantidade": 3},
-    {"produto": "Sal", "preco": 2.00, "quantidade": 4},
-    {"produto": "Açúcar", "preco": 2.00, "quantidade": 3},
-    {"produto": "Farinha", "preco": 2.00, "quantidade": 5},
-    {"produto": "Cereal matinal", "preco": 5.00, "quantidade": 2},
-    {"produto": "Aveia", "preco": 2.00, "quantidade": 3},
-    {"produto": "Mel", "preco": 15.00, "quantidade": 2},
-    {"produto": "Ketchup", "preco": 3.00, "quantidade": 2},
-    {"produto": "Mustarda", "preco": 2.00, "quantidade": 3},
-    {"produto": "Maionese", "preco": 3.00, "quantidade": 3},
-    {"produto": "Molho de soja", "preco": 3.00, "quantidade": 3},
-]
+import matplotlib.pyplot as plt
+import os
+import time
 
-def adicionarQuantidade(mercadorias):
-    for i in range(len(mercadorias)):
-        mercadorias[i]['quantidade'] = input("Quantidade de vendas de " + mercadorias[i]['produto'] + ": ")
-        
-    print(mercadorias[0]['quantidade'])
-        
+precos = {1: 10.0, 2: 15.0, 3: 20.0, 4: 25.0, 5: 30.0, 6: 35.0, 7: 40.0, 8: 45.0, 9: 50.0, 10: 55.0}
 
-while True:
-    print(
-        """
-|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|
-|                    MENU                     |
-|---------------------------------------------|
-|                                             |
-|   1 - Adicionar quantidade de um produto    |
-|                                             |
-|_____________________________________________|
-        """
-    )
+quantidades_vendidas = {}
 
-    opcao = int(input("Digite alguma opção: "))
+def ler_dados():
+    for i in range(1, 11):
+        quantidade = int(input(f"Digite a quantidade vendida da mercadoria {i}: "))
+        quantidades_vendidas[i] = quantidade
+
+def calcular_faturamento():
+    faturamento = 0.0
+    for mercadoria, quantidade in quantidades_vendidas.items():
+        faturamento += quantidade * precos[mercadoria]
+    return faturamento
+
+def imprimir_faturamento():
+    faturamento = 0.0
+    for mercadoria, quantidade in quantidades_vendidas.items():
+        faturamento += quantidade * precos[mercadoria]
+    print("Faturamento mensal do armazém:")
+    for mercadoria, quantidade in quantidades_vendidas.items():
+        print(f"Mercadoria {mercadoria}: Quantidade vendida = {quantidade}, Preço = {precos[mercadoria]}")
+    print(f"Total Faturado: {faturamento}")
+
+def calcular_percentuais():
+    faturamento = calcular_faturamento()
+    percentuais = {k: (v * precos[k] / faturamento) * 100 for k, v in quantidades_vendidas.items()}
+    return percentuais
+
+def imprimir_percentuais():
+    percentuais = calcular_percentuais()
+    print("Percentuais de vendas por mercadoria:")
+    for mercadoria, percentual in percentuais.items():
+        print(f"Mercadoria {mercadoria}: {percentual:.2f}% do faturamento")
+
+def gravar_em_arquivo():
+    with open("armazem/vendas.txt", "w") as arquivo:
+        arquivo.write("Mercadoria,Quantidade Vendida,Preço\n")
+        for mercadoria, quantidade in quantidades_vendidas.items():
+            arquivo.write(f"{mercadoria},{quantidade},{precos[mercadoria]}\n")
+
+def gerar_grafico():
+    sorted_vendas = sorted(quantidades_vendidas.items(), key=lambda x: x[1], reverse=True)
+    top5 = sorted_vendas[:5]
+    mercadorias, quantidades = zip(*top5)
+    plt.bar(mercadorias, quantidades)
+    plt.xlabel("Mercadorias")
+    plt.ylabel("Quantidade Vendida")
+    plt.title("Top 5 Mercadorias Mais Vendidas")
+    plt.show()
     
-    if opcao == 1 :
-        adicionarQuantidade(mercadorias)
+def loading(tempo) :
+    value = 0
+    while value <= tempo :
+        print("")
+        time.sleep(0.08)
+        os.system('clear')
+        print("/")
+        time.sleep(0.08)
+        os.system('clear')
+        print("--")
+        time.sleep(0.08)
+        os.system('clear')
+        print("\x5c")
+        time.sleep(0.08)
+        os.system('clear')
+        value += 1
+
+def menu():
+    while True:
+        print(
+        """
+|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|
+|                           MENU                          |
+|---------------------------------------------------------|
+|                                                         |
+|  1 - Registrar quantidade de vendas                     |
+|  2 - Imprimir faturamento                               |
+|  3 - Imprimir percentuais de vendas                     |
+|  4 - Gravar dados em arquivo                            |
+|  5 - Gerar gráfico das cinco mercadorias mais vendidas  |
+|  0 - Fechar o programa                                  |
+|                                                         |
+|_________________________________________________________|
+        """
+        )
+        
+        opcao = int(input("Escolha uma opção: "))
+        
+        if opcao == 1:
+            os.system('clear')
+            ler_dados()
+            loading(3)
+            os.system('clear')
+            print("\nCadastrado com sucesso!")
+            time.sleep(2)
+            os.system('clear')
+            
+        elif opcao == 2:
+            os.system('clear')
+            loading(3)
+            os.system('clear')
+            imprimir_faturamento()
+            time.sleep(8)
+            os.system('clear')
+            
+        elif opcao == 3:
+            os.system('clear')
+            loading(3)
+            os.system('clear')
+            imprimir_percentuais()
+            time.sleep(6)
+            os.system('clear')
+        
+        elif opcao == 4:
+            os.system('clear')
+            gravar_em_arquivo()
+            loading(3)
+            os.system('clear')
+        
+        elif opcao == 5:
+            os.system('clear')
+            loading(3)
+            os.system('clear')
+            gerar_grafico()
+            time.sleep(8)
+            os.system('clear')
+        
+        elif opcao == 0:
+            break
+        
+        else:
+            print("Opção inválida. Tente novamente.")
+
+menu()
